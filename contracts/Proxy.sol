a9ee66618b94dd6922f2e9f8a53713af64670336
BappaBanerjee <bappa3727@gmail.com>
2024-02-24 02:33:25 +0530

    comments added

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -4,9 +4,11 @@ import "./StorageSlot.sol";
 
 contract Proxy{
 
+    // Custom errors
     error Implementation_NotFound();
     error Invalid_Contract_Address();
 
+    // Storage slots for admin and implementation addresses
     bytes32 private constant ADMIN_SLOT = bytes32(uint256(
             keccak256("eip1967.proxy.admin")) - 1
         );
@@ -15,6 +17,7 @@ contract Proxy{
             keccak256("eip1967.proxy.implementation")) - 1
         );
 
+    // Modifier to check if the sender is the admin
     modifier isAdmin() {
         if(msg.sender == _getAdmin()){
             _;
@@ -23,23 +26,28 @@ contract Proxy{
         }
     }
 
+    // Constructor sets the deployer as the admin
     constructor() {
         _setAdmin(msg.sender);
     }
 
+    // Internal function to set the admin address in storage
     function _setAdmin(address _addr) private {
         StorageSlot.getAddressSlot(ADMIN_SLOT).value[bytes4(keccak256("admin"))] = _addr;
     }
 
+    // Internal function to get the admin address from storage
     function _getAdmin() private view returns(address) {
         return StorageSlot.getAddressSlot(ADMIN_SLOT).value[bytes4(keccak256("admin"))];
     }
 
+    // Internal function to set the implementation address for a function in storage
     function _setImplementation(bytes4 _functionId, address _implementation) private {
         if(_implementation == address(0)) revert Invalid_Contract_Address();
         StorageSlot.getAddressSlot(IMPLEMENTATION_SLOT).value[_functionId] = _implementation;
     }
 
+    // Internal function to get the implementation address for a function from storage
     function _getImplementation(bytes4 _functionId) private view returns (address) {
         return StorageSlot.getAddressSlot(IMPLEMENTATION_SLOT).value[_functionId];
     }
@@ -47,34 +55,42 @@ contract Proxy{
     //admin interface
 
     function upgradeTo(bytes4 _functionId, address _implementation) external isAdmin   {
+        // Upgrade implementation for a specific function
         _setImplementation(_functionId, _implementation);
     }
 
     function changeAdmin(address _admin) public isAdmin {
+         // Change the admin address
         _setAdmin(_admin);
     }
 
     function admin() external isAdmin returns (address) {
+        // Get the admin address
         return _getAdmin();
     }
 
     function implementation(bytes4 _functionId) external isAdmin returns (address) {
+        // Get the implementation address for a specific function
         return _getImplementation(_functionId);
     }
 
     function deleteImplementation(bytes4 _functionId) public isAdmin{
+         // Delete the implementation address for a specific function
         delete StorageSlot.getAddressSlot(IMPLEMENTATION_SLOT).value[_functionId];
     }
 
     //user Interface
     fallback() external payable { 
+        // Fallback function redirects the call to the appropriate implementation
         _fallback();
     }
 
     receive() external payable { 
+        // Receive function redirects the call to the appropriate implementation
         _fallback();
     }
 
+    // Internal function to handle fallback logic
     function _fallback() private  {
         bytes4 functionId = _getFunctionId(msg.data);
         address _implementation = _getImplementation(functionId);
@@ -82,6 +98,7 @@ contract Proxy{
         _delegate(_implementation);
     }
 
+    // Internal function to extract the function ID from calldata
     function _getFunctionId(bytes memory data) private pure returns (bytes4) {
         bytes4 functionId;
         assembly {
@@ -90,6 +107,7 @@ contract Proxy{
         return functionId;
     }
 
+    // Internal function to delegate call to the implementation
     function _delegate(address _implementation) internal virtual {
         assembly {
             
--- contracts/ProxyAdmin.sol
+++ contracts/ProxyAdmin.sol
@@ -4,29 +4,36 @@ pragma solidity 0.8;
 import "./Proxy.sol";
 
 contract ProxyAdmin {
+    // Custom error for unauthorized access
     error NOT_AUTHORIZED();
 
+     // Owner of the ProxyAdmin contract
     address public owner;
 
+    // Constructor sets the deployer as the owner
     constructor() {
         owner = msg.sender;
     }
 
+    // Modifier to ensure that only the owner can execute a function
     modifier onlyOwner() {
         if(msg.sender != owner) revert NOT_AUTHORIZED();
         _;
     }
 
+    // Get the admin address of a proxy contract
     function getProxyAdmin(address proxy) external view returns (address) {
         (bool ok, bytes memory res) = proxy.staticcall(abi.encodeCall(Proxy.admin, ()));
         require(ok, "call failed");
         return abi.decode(res, (address));
     }
 
+    // Change the admin address of a proxy contract
     function changeProxyAdmin(address payable proxy, address admin) external onlyOwner {
         Proxy(proxy).changeAdmin(admin);
-    }
+    }   
 
+    // Get the implementation address for a specific function in a proxy contract
     function getProxyImplementation(address proxy, bytes4 functionId) external view returns (address) {
        (bool ok, bytes memory res) = proxy.staticcall(
             abi.encodeCall(Proxy.implementation, (functionId))
@@ -35,10 +42,12 @@ contract ProxyAdmin {
         return abi.decode(res, (address));
     }
 
+    // Upgrade the implementation address for a specific function in a proxy contract
     function upgradeImplementation(address payable proxy, address implementation, bytes4 functionId) external onlyOwner {
         Proxy(proxy).upgradeTo(functionId, implementation);
     }
 
+    // Delete the implementation address for a specific function in a proxy contract
     function deleteImplementation(address payable proxy, bytes4 functionId) external onlyOwner {
         Proxy(proxy).deleteImplementation(functionId);
     }
